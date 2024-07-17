37cf47e1a6e36ab621f66514505faaaed7448a5e
alfiansyah <alfian.official.mail@gmail.com>
2024-07-17 08:06:43 +0700

    TheMovieDBCore

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Core",
+            name: "TheMovieDBCore",
             targets: ["Core"]),
     ],
     dependencies: [
