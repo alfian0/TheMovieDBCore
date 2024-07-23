1f6f90c870059aadbdd58a2331d09967e44a9890
alfiansyah <alfian.official.mail@gmail.com>
2024-07-23 19:36:17 +0700

    Load container

--- Sources/TheMovieDBCore/Utils/Bundle+Extension.swift
+++ Sources/TheMovieDBCore/Utils/Bundle+Extension.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import CoreData
 
 extension Bundle {
   public func loadAndDecodeJSON<D: Decodable>(filename: String) throws -> D {
@@ -18,5 +19,16 @@ extension Bundle {
     let jsonDecoder = Utils.jsonDecoder
     let decodeModel = try jsonDecoder.decode(D.self, from: data)
     return decodeModel
+  }
+  
+  public func loadNSPersistentContainer(filename: String) throws -> NSPersistentContainer {
+    guard let url = self.url(forResource: filename, withExtension: "momd") else {
+      throw NSError(domain: "FileNotFound",
+                    code: 404,
+                    userInfo: [NSLocalizedDescriptionKey: "File \(filename).json not found in bundle."])
+    }
+    let managedObjectModel =  NSManagedObjectModel(contentsOf: url)!
+    let container = NSPersistentContainer(name: filename, managedObjectModel: managedObjectModel)
+    return container
   }
 }
