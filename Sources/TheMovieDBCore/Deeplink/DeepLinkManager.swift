7ec0a09ff964877b444ace202e8c796511d96874
alfiansyah <alfian.official.mail@gmail.com>
2024-08-02 15:27:34 +0700

    To Singleton

--- Sources/TheMovieDBCore/Deeplink/DeepLinkManager.swift
+++ Sources/TheMovieDBCore/Deeplink/DeepLinkManager.swift
@@ -17,12 +17,22 @@ public protocol DeepLinkManager {
 }
 
 public class DefaultDeepLinkManager: DeepLinkManager {
+  
+  // Static shared instance
+  public static let shared = DefaultDeepLinkManager()
+  
+  // Private array to hold deep links
   private var deepLinks: [DeepLink] = []
-    
-  public init(deepLinks: [DeepLink]) {
+  
+  // Private initializer to prevent multiple instances
+  private init() {}
+  
+  // Method to set deep links, if needed
+  public func setDeepLinks(_ deepLinks: [DeepLink]) {
     self.deepLinks = deepLinks
   }
-    
+  
+  // Method to handle deep links
   public func handleDeepLink(url: URL) {
     guard let deepLink = deepLinks.first(where: { $0.path == url.host }) else {
       print("No handler for URL: \(url)")
